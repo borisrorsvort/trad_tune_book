11da339a7145c15686ad953ef84a9d7b9f7b5470
Boris Rorsvort <boris.rorsvort@gmail.com>
2017-08-15 12:10:22 +0200

    First render of music sheet and split pane

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SplitPane from 'react-split-pane';
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import ABCJS from 'exports-loader?ABCJS!script-loader!./../node_modules/abcjs/bin/abcjs_basic_2.3-min.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      tune: 'X:1\nT: Cooley\'s\nM: 4/4\nL: 1/8\nR: reel\nK: Emin\nD2|:"Em"EB{c}BA B2 EB|~B2 AB dBAG|"D"FDAD BDAD|FDAD dAFD|\n"Em"EBBA B2 EB|B2 AB defg|"D"afe^c dBAF|1"Em"DEFD E2 D2:|2"Em"DEFD E2 gf||\n|:"Em"eB B2 efge|eB B2 gedB|"D"A2 FA DAFA|A2 FA defg|\n"Em"eB B2 eBgB|eB B2 defg|"D"afe^c dBAF|1"Em"DEFD E2 gf:|2"Em"DEFD E4|]\n'
+    }
+  }
+
+  componentDidMount() {
+    this.renderMusicSheet(this.state.tune)
+  }
+
+  renderMusicSheet(tune) {
+    ABCJS.renderAbc('music-sheet', tune)
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to BOris</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <SplitPane split="vertical" minSize={250}>
+          <div>arsuiet</div>
+          <div id='music-sheet'></div>
+      </SplitPane>
     );
   }
 }
