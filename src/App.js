d8e9bac88b937a7447af592043ff1a9965d1fee2
Boris Rorsvort <boris.rorsvort@gmail.com>
2017-08-16 08:50:21 +0200

    Add material ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import SplitPane from 'react-split-pane';
+
+import { withStyles } from 'material-ui/styles';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import Button from 'material-ui/Button';
+import IconButton from 'material-ui/IconButton';
+import MenuIcon from 'material-ui-icons/Menu';
+import Tabs from 'material-ui/Tabs/Tabs';
+import Tab from 'material-ui/Tabs/Tab';
+
+
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import ABCJS from 'exports-loader?ABCJS!script-loader!./../node_modules/abcjs/bin/abcjs_basic_2.3-min.js';
+
+const styles = {
+  root: {
+    width: '100%',
+  },
+  flex: {
+    flex: 1,
+  },
+};
 
 class App extends Component {
   constructor(props) {
@@ -22,13 +42,33 @@ class App extends Component {
   }
 
   render() {
+    const classes = this.props.classes;
     return (
-      <SplitPane split="vertical" minSize={250}>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton color="contrast" aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography type="title" color="inherit" className={classes.flex}>
+              TradTuneBook
+            </Typography>
+            <Button color="contrast">Login</Button>
+          </Toolbar>
+          <Tabs style={styles.tabs} initialSelectedIndex={0}>
+            <Tab value={0} label="home"/>
+            <Tab value={1} label="about"/>
+            <Tab value={2} label="contact"/>
+            <Tab value={3} label="faq"/>
+          </Tabs>
+        </AppBar>
+        <SplitPane split="vertical" minSize={250}>
           <div>arsuiet</div>
           <div id='music-sheet'></div>
-      </SplitPane>
+        </SplitPane>
+      </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
