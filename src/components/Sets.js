005ef0f38ee588ee7b0672ee6850ab858be0454e
Boris Rorsvort <boris.rorsvort@gmail.com>
2017-08-20 21:21:49 +0200

    Add sets list

--- src/components/Sets.js
+++ src/components/Sets.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { withStyles } from 'material-ui';
+import SplitPane from 'react-split-pane';
+import he from 'he';
+import {
+  withStyles,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  List,
+} from 'material-ui';
 import { fetchSets } from '../actions/sets';
 import store from '../store';
+import { MusicNote } from 'material-ui-icons';
+import { Fragment } from 'redux-little-router';
+import { redirect } from '../actions/router';
 
 const styles = theme => ({
   flex: {
@@ -14,8 +25,32 @@ class Sets extends Component {
   componentDidMount() {
     store.dispatch(fetchSets('63117'));
   }
+
+  handleClick = href => {
+    store.dispatch(redirect(href));
+  };
+
   render() {
-    return <div>sets</div>;
+    return (
+      <SplitPane split="vertical" minSize={250}>
+        <List>
+          {this.props.sets.map(set =>
+            <ListItem
+              key={set.id}
+              className={this.props.classes.button}
+              button
+              onClick={item => this.handleClick(`/sets/${set.id}`)}
+            >
+              <ListItemIcon>
+                <MusicNote />
+              </ListItemIcon>
+              <ListItemText primary={he.decode(set.name)} />
+            </ListItem>,
+          )}
+        </List>
+        <Fragment forRoute="/:tuneId">aunisret</Fragment>
+      </SplitPane>
+    );
   }
 }
 
