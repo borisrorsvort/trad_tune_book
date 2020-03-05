30f7e31a88130d1e8a939fe22aea808ee81d2a58
Boris Rorsvort <boris@rorsvort.com>
2020-03-05 20:42:36 +0100

    Cleanup

--- src/components/Tune.js
+++ src/components/Tune.js
@@ -11,7 +11,7 @@ import {
 } from "@material-ui/core";
 import React, { Component } from "react";
 
-import { Link, withRouter } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 import SheetMusic from "./SheetMusic";
 import { TUNE_URL } from "../constants/actionTypes";
 import { connect } from "react-redux";
