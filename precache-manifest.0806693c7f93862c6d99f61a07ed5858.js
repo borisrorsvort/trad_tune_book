fad0ff4298e71ccfa69bc0c33cbf71dbf4113307
Boris Rorsvort <boris@rorsvort.com>
2020-10-25 12:09:51 +0100

    Updates

---
+++ precache-manifest.0806693c7f93862c6d99f61a07ed5858.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea10ed1d0b95de0ae0c7fc33e7482e16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5737e7e25f14c0508692",
+    "url": "/static/css/main.c51b0b1f.chunk.css"
+  },
+  {
+    "revision": "549f7e6f531071165823",
+    "url": "/static/js/2.8d4c9981.chunk.js"
+  },
+  {
+    "revision": "2ca76ed54bf70ecbaf5df18ec50b478c",
+    "url": "/static/js/2.8d4c9981.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5737e7e25f14c0508692",
+    "url": "/static/js/main.3f25cd30.chunk.js"
+  },
+  {
+    "revision": "3205540889dd495690fd",
+    "url": "/static/js/runtime-main.56e958e2.js"
+  },
+  {
+    "revision": "0ff303d1347b3d9b646802d41d953e5a",
+    "url": "/static/media/logo-small.0ff303d1.svg"
+  },
+  {
+    "revision": "6f584227521b07d0c8d542197e52aa06",
+    "url": "/static/media/logo.6f584227.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3d844dabfe7feabd8bc8fc131f6962fb.js"
+  "/precache-manifest.0806693c7f93862c6d99f61a07ed5858.js"
 );
 
 self.addEventListener('message', (event) => {
